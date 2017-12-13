64d8f1fd4cde4c67efbdefa18e1c4da24c699a32
panduanlove <panduanlove@163.com>
2017-12-13 23:16:47 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script type="text/ecmascript-6">
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/variable"
+
+  #app
+    color:$color-theme
+</style>

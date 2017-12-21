cec8fd0ebd06685072f87aaff9ff4a1993fa4db9
panduanlove <panduanlove@163.com>
2017-12-22 00:06:13 +0800

    功能：增加热门歌单推荐及垂直滚动

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
+import VueLazyLoad from 'vue-lazyload'
 
 // Vue.config.productionTip = false
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({

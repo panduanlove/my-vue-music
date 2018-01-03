7d723f71bcda090546a4a1ec248b767d4601579c
panduanlove <panduanlove@163.com>
2018-01-03 22:38:29 +0800

    功能：增加歌曲列表页面

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
   </transition>  
 </template>
 
@@ -9,13 +9,20 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
   export default {
     data() {
       return {
-        song: []
+        songs: []
       }
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -46,6 +53,9 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
@@ -53,15 +63,6 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
   
-  .singer-detail
-    position:fixed
-    z-index:100
-    top:0
-    left:0
-    right:0
-    bottom:0
-    background:$color-background
-
   .slide-enter-active,.slide-leave-active
     transition:all 0.3s
   .slide-enter,.slide-leave-to

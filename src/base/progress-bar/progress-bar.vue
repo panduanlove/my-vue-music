fe297a6b5489618a137e29d8b30042602140160f
panduanlove <panduanlove@163.com>
2018-01-15 22:16:56 +0800

    功能：播放器播放模式切换实现

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -47,7 +47,11 @@ export default {
       this._triggerPercent()
     },
     progressClick(e) {
-      this._offset(e.offsetX)
+      const rect = this.$refs.progressBar.getBoundingClientRect()
+      const offsetWidth = e.pageX - rect.left
+      this._offset(offsetWidth)
+      // this._offset(e.offsetX)
+      // 这里当我们点击processBtn的时候e.offsetX获取不对
       this._triggerPercent()
     },
     _offset(offsetWidth) {
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -80,6 +80,7 @@
           @canplay="ready" 
           @error="error"
           @timeupdate="updateTime"
+          @ended="end"
     ></audio>
   </div>
 </template>
@@ -91,6 +92,7 @@
   import ProgressBar from 'base/progress-bar/progress-bar'
   import ProgressCircle from 'base/progress-circle/progress-circle'
   import {playMode} from 'common/js/config'
+  import {shuffle} from 'common/js/util'
 
   const transform = prefixStyle('transform')
   
@@ -127,7 +129,8 @@
         'currentSong',
         'playing',
         'currentIndex',
-        'mode'
+        'mode',
+        'sequenceList'
       ])
     },
     methods: {
@@ -183,6 +186,17 @@
         }
         this.setPlayingState(!this.playing)
       },
+      end() {
+        if (this.mode === playMode.loop) {
+          this.loop()
+        } else {
+          this.next()
+        }
+      },
+      loop() {
+        this.$refs.audio.currentTime = 0
+        this.$refs.audio.play()
+      },
       next() {
         if (!this.songReady) {
           return
@@ -236,6 +250,20 @@
       changeMode() {
         const mode = (this.mode + 1) % 3
         this.setPlayMode(mode)
+        let list = null
+        if (mode === playMode.random) {
+          list = shuffle(this.sequenceList)
+        } else {
+          list = this.sequenceList
+        }
+        this.resetCurrentIndex(list)
+        this.setPlayList(list)
+      },
+      resetCurrentIndex(list) {
+        let index = list.findIndex((item) => {
+          return item.id === this.currentSong.id
+        })
+        this.setCurrentIndex(index)
       },
       _pad(num, n = 2) {
         let len = num.toString().length
@@ -264,11 +292,15 @@
         setFullScreen: 'SET_FULL_SCREEN',
         setPlayingState: 'SET_PLAYING_STATE',
         setCurrentIndex: 'SET_CURRENT_INDEX',
-        setPlayMode: 'SET_PLAY_MODE'
+        setPlayMode: 'SET_PLAY_MODE',
+        setPlayList: 'SET_PLAYLIST'
       })
     },
     watch: {
-      currentSong() {
+      currentSong(newSong, oldSong) {
+        if (newSong.id === oldSong.id) {
+          return
+        }
         this.$nextTick(() => {
           this.$refs.audio.play()
         })
